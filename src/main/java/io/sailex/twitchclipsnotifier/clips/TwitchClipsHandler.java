fa676c30e3497655218aebd7a4fb97114abf190c
sailex <elias.hahn@mail.de>
2024-08-19 20:18:00 +0200

    fix: wrong thread pool, added dev.properties

--- src/main/java/io/sailex/twitchclipsnotifier/clips/TwitchClipsHandler.java
+++ src/main/java/io/sailex/twitchclipsnotifier/clips/TwitchClipsHandler.java
@@ -21,7 +21,7 @@ public class TwitchClipsHandler {
 
     private final List<Clip> currentClips = new ArrayList<>();
 
-    private final ScheduledExecutorService executor = Executors.newScheduledThreadPool(10);
+    private final ScheduledExecutorService executor = Executors.newScheduledThreadPool(5);
 
     private final NotificationBot notificationBot;
 
